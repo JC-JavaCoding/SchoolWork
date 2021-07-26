e472f844a57be0098cdaf368b0cfa835998fa1c3
JanChristiaanCode <84249218+JC-JavaCoding@users.noreply.github.com>
2021-07-26 21:47:00 +0200

    Finish homework on the compareTo, toUpperCase methods.

--- Reddam/src/main/java/Chars_Strings/ToUpperCase_LowerCase_Compare.java
+++ Reddam/src/main/java/Chars_Strings/ToUpperCase_LowerCase_Compare.java
@@ -5,6 +5,8 @@
  */
 package Chars_Strings;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jjaac
@@ -13,6 +15,27 @@ public class ToUpperCase_LowerCase_Compare
 {
     public static void main(String[] args)
     {
-        String word1 = "Hello";
+        String word1 = JOptionPane.showInputDialog("First word.");
+        String word2 = JOptionPane.showInputDialog("Second word.");
+        String alphabetical = "";
+        int result = word1.compareTo(word2);
+        
+        if (result > 0)
+        {
+            alphabetical = word2;
+        }
+        else if (result < 0)
+        {
+            alphabetical = word1;
+        }
+        else 
+        {
+            System.out.println("Your words are the same you fool!");
+            alphabetical = word1;
+        }
+        
+        System.out.println("Word: \""+ alphabetical +"\" is more alphabetical.");
+        alphabetical = alphabetical.toUpperCase();
+        System.out.println(alphabetical);
     }
 }
