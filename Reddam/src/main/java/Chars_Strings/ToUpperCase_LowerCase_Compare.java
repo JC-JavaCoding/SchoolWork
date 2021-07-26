40299bc7e96ab67d014170b2db68e00bf799f036
JanChristiaanCode <84249218+JC-JavaCoding@users.noreply.github.com>
2021-07-26 21:23:25 +0200

    Testing the commit function

---
+++ Reddam/src/main/java/Chars_Strings/ToUpperCase_LowerCase_Compare.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chars_Strings;
+
+/**
+ *
+ * @author jjaac
+ */
+public class ToUpperCase_LowerCase_Compare
+{
+    public static void main(String[] args)
+    {
+        String word1 = "Hello";
+    }
+}
