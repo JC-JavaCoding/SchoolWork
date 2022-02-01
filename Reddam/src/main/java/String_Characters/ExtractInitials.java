145c4c541263546de1ef6be4472575fe2ec4cadc
janch <janch@192.168.0.105>
2022-02-01 19:26:52 +0200

    hw from home

--- Reddam/src/main/java/String_Characters/ExtractInitials.java
+++ Reddam/src/main/java/String_Characters/ExtractInitials.java
@@ -15,8 +15,34 @@ public class ExtractInitials
     public static void main(String[] args)
     {
         String input = JOptionPane.showInputDialog("Enter full name");
+        
+        System.out.println("Initials: "+ getInitials(input));
+    }
+    private static String getInitials(String input)
+    {
         String initials = "";
+        String surname = getSurname(input);
+        int prevBlankIndx = 0;
         
+        while (input.contains(" ") )//&& input.indexOf(" ") != input.lastIndexOf(" "))
+        {
+            initials += input.charAt(0);
+            System.out.println("charAt: "+ input.charAt(prevBlankIndx));
+            input = input.substring(input.indexOf(" ")+1);
+        }
+        initials+= surname;
+        initials = initials.toUpperCase();
+        return initials;
+    }
+    private static String getSurname(String input)
+    {
+        String surname ="";
         
+        if(input.contains(" "))
+        {
+            surname = input.substring(input.lastIndexOf(" "));
+        }
+        
+        return surname;
     }
 }
