7fff151fbded1ce697946eb7f51e4dcc9d0819b9
Jcj <Jcj@ZAKZRHB-CLLB12.ballito.local>
2022-05-06 10:46:13 +0200

    Improved bubble sort

--- Reddam/src/main/java/Arrays/SortArrasy.java
+++ Reddam/src/main/java/Arrays/SortArrasy.java
@@ -21,6 +21,10 @@ public class SortArrasy
         //DISPLAY:
         System.out.println(Arrays.toString(arr));
     }
+    
+    /*
+    BUBBLE SORT
+    */
     static void bubbleSort(int [] arr)
     {
         for (int sortedIndx = arr.length -1; sortedIndx >= 0; sortedIndx --)
@@ -40,7 +44,34 @@ public class SortArrasy
             }
         }
     }
-    
+    /*
+    IMPROVED BUBBLE SORT
+    */
+      static void improvedBubbleSort(int [] arr)
+    {
+        for (int sortedIndx = arr.length -1; sortedIndx >= 0; sortedIndx --)
+        {
+            boolean sorted = true;
+            improvedBubbleSecond(arr, sortedIndx, sorted);
+            if (sorted)break;
+        }
+    }
+    static void improvedBubbleSecond(int [] arr,int sortedIndx, boolean sorted)
+    {
+        for (int i = 0; i < sortedIndx; i ++)
+        {
+            if (arr[i] > arr[sortedIndx])
+            {
+                int temp = arr[i];
+                arr[i] = arr[sortedIndx];
+                arr[sortedIndx] = temp;
+                sorted = false;
+            }
+        }
+    }
+    /*
+    SLECTION SORT
+    */
     static void selectionSort(int [] arr)
     {
         for (int startIndex = 0; startIndex < arr.length; startIndex++)
@@ -51,13 +82,23 @@ public class SortArrasy
     static void selectionSecond(int[] arr, int startIndex)
     {
         for (int i = startIndex+1; i < arr.length; i++)
+        {
+            if(arr[i] < arr[startIndex])
             {
-                if(arr[i] < arr[startIndex])
-                {
-                    int temp = arr[i];
-                    arr[i] = arr[startIndex];
-                    arr[startIndex] = temp;
-                }
+                int temp = arr[i];
+                arr[i] = arr[startIndex];
+                arr[startIndex] = temp;
             }
+        }
+    }
+    /*
+    IMPROVED  SELECTION SORT
+    */
+    static void improvedSelectionSort(int [] arr)
+    {
+        for (int startIndex = 0; startIndex < arr.length; startIndex++)
+        {
+            selectionSecond(arr, startIndex);
+        }
     }
 }
